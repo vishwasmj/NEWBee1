fb41f6f9dabd4841ccf775f27529a34003e14e4b
vishwasmj <kaipuvishwas7@gmail.com>
2017-06-19 10:13:42 +0200

    Add files via upload

---
+++ Help_description.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include "Help_description.hpp"
+
+using namespace std;
+using namespace cv;
+void Help_description::run(){
+	cout<<"\t\t Help_description \n"<<endl;
+	cout<<"1.If you press one the resize option is selected and you can resize the image which you have selected\n"<<endl;
+
+	cout<<"2.If you press 2 the Erosion option is selected and you can perform the ersion operation\n"<<endl;
+	
+	cout<<"3.If you press 3 the Dilation option is selected and you can perform the dilation operation\n"<<endl;
+	
+	cout<<"4.If you press 4 the Lighten option is selected and you can perform the lighten operation\n"<<endl;
+	
+cout<<"5.If you press 5 the Darken option is selected and you can perform the darken operation\n"<<endl;
+	
+cout<<"6.If you press 6 the Panaroma/Stiching option is selected and you can perform the Panaroma/Stiching operation\n"<<endl;
+
+cout<<"6.If you press 6 the Panaroma/Stiching option is selected and you can perform the Panaroma/Stiching operation\n"<<endl;
+	
+
+
+  }
---
+++ Help_description.hpp
@@ -0,0 +1,17 @@
+#ifndef HELP_H_
+#define HELP_H_
+
+#include "Action.hpp"
+#pragma once
+
+using namespace cv;
+
+class Help_description: public Action {
+
+public:
+virtual void run();
+};
+
+
+#endif
+
